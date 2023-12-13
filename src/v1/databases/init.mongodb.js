a0776e83b0c071db05f25da824e243fcfb69346f
Tran Tinh <tinhtranngoc@itrvn.com>
2023-12-13 16:12:51 +0700

    deploy

--- src/v1/databases/init.mongodb.js
+++ src/v1/databases/init.mongodb.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-//connect mongoose
-mongoose.connect( process.env.MONGO_URI).then( _ => console.log('Connected mongoose success!...'))
-.catch( err => console.error(`Error: connect:::`, err))
+// connect mongoose
+mongoose.connect(process.env.MONGO_URI).then(_ => console.log('Connected mongoose success!...'))
+  .catch(err => console.error('Error: connect:::', err));
 
 // all executed methods log output to console
-mongoose.set('debug', true)
-    
+mongoose.set('debug', true);
+
 // disable colors in debug mode
-mongoose.set('debug', { color: false })
+mongoose.set('debug', { color: false });
 
 // get mongodb-shell friendly output (ISODate)
-mongoose.set('debug', { shell: true })
-
+mongoose.set('debug', { shell: true });
 
 module.exports = mongoose;
--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,32 +1,33 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    message: 'api ok',
+  });
+});
 
 router.get('/api/users', (req, res, next) => {
-    res.status(200).json({
-        status: 'success api',
-        message: 'api ok',
-        metadata: [
-            {
-                name: 'anonystick',
-                age: 40
-            },
-            {
-                name: 'Ronaldo',
-                age: 39
-            },
-            {
-                name: 'Messi',
-                age: 37
-            }
-        ]
-    })
-})
+  res.status(200).json({
+    status: 'success api',
+    message: 'api ok',
+    metadata: [
+      {
+        name: 'anonystickss',
+        age: 40,
+      },
+      {
+        name: 'Ronaldo',
+        age: 39,
+      },
+      {
+        name: 'Messi',
+        age: 37,
+      },
+    ],
+  });
+});
 
 module.exports = router;
